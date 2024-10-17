da161cb117f2886bc19ae953e42b1e266b755c04
Mayukh Basu <roxy.firebird@gmail.com>
2024-10-17 18:58:31 +0530

    implemented sso in api gateway

--- UI/trello-ui/src/pages/HomePage.tsx
+++ UI/trello-ui/src/pages/HomePage.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import './HomePage.css'; // Add custom styles
 import { Container, Button } from 'react-bootstrap';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
+const clientId = '104926798924-0m8a4e0l1elhus477miu6er0n8r1forf.apps.googleusercontent.com'; // Replace with your actual Google Client ID
 
 const HomePage: React.FC = () => {
+
+  const handleLoginSuccess = (credentialResponse: any) => {
+    const token = credentialResponse.credential;
+    localStorage.setItem('access_token', token);
+  };
+
+  const handleLoginError = () => {
+    console.log('Login Failed');
+  };
+
   return (
-    <>
+    <GoogleOAuthProvider clientId={clientId}>
       <Container>
         <h1 className="mb-4">Welcome to Trello</h1>
         <div className="button-group">
-          <Button href="#sign-in" variant="outline-light" size="lg" className="me-3">
-            Sign In
-          </Button>
-          <Button href="#sign-up" variant="primary" size="lg">
-            Sign Up
-          </Button>
+          <div className="google-login-wrapper">
+            <GoogleLogin
+              onSuccess={handleLoginSuccess}
+              onError={handleLoginError}
+              size="large"
+              text="continue_with"
+            />
+          </div>
         </div>
       </Container>
-    </>
+    </GoogleOAuthProvider>
   );
 };
 

00ab9ec1734204d08721e086090a25f71f5c2075
Mayukh Basu <roxy.firebird@gmail.com>
2024-10-17 08:54:09 +0530

    navbar added

--- UI/trello-ui/src/App.tsx
+++ UI/trello-ui/src/App.tsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import './App.css';
 import Users from './Users';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <Users />
       </header>

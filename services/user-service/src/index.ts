37477e19f7c8f33ac67a445663919387b90ea9e5
Mayukh Basu <roxy.firebird@gmail.com>
2024-10-18 12:29:29 +0530

    changes for user backend

--- services/user-service/src/index.ts
+++ services/user-service/src/index.ts
@@ -5,19 +5,23 @@ import { ApolloServer } from 'apollo-server';
 import { connectToMongoDB } from './database';
 
 async function startServer() {
+  await connectToMongoDB();
   const schema = await buildSchema({
     resolvers: [UserResolver]
   });
-  await connectToMongoDB();
+  
   // Adding CORS configuration to the Apollo Server
   const server = new ApolloServer({
     schema,
     cors: {
-      origin: ['http://localhost:3000'], // Allow specific origins
-      credentials: true, // Allow credentials such as cookies or authentication headers
+      origin: [
+        'http://localhost:3000', // Allow your local frontend (if applicable)
+        'https://studio.apollographql.com', // Allow Apollo Studio
+      ],
+      credentials: true, // Allow credentials like cookies and authentication headers
     },
+    introspection: true, // Allow introspection (optional in dev)
   });
-
   const port = process.env.PORT || 4001;
   server.listen({ port }).then(({ url }) => {
     console.log(`🚀 User Service ready at ${url}`);

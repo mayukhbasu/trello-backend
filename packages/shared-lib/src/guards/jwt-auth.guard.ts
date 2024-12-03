7659ac2e01bca73249083a75cdcf3aebfb42cb9f
Mayukh Basu <roxy.firebird@gmail.com>
2024-12-03 11:00:09 +0530

    updated trello backend

--- packages/shared-lib/src/guards/jwt-auth.guard.ts
+++ packages/shared-lib/src/guards/jwt-auth.guard.ts
@@ -1,6 +1,6 @@
 import { OAuth2Client } from 'google-auth-library';
 
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, Logger} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
